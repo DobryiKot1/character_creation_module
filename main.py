171fad78931c859c9276a896008256b8938879b2
Александр Лобанов <lo.aleks2015@yandex.ru>
2022-10-28 00:44:34 +0300

    FULL

--- main.py
+++ main.py
@@ -63,38 +63,26 @@ class Healer(Character):
     SPECIAL_SKILL = 'Защита'
 
 
-
-
-# Новый импорт.
-# Из модуля start_game_banner, которxый расположен в папке graphic_arts,
-# импортируем функцию run_screensaver().
-
-
-def start_training(char_name: str, char_class: Character):
-    """Ведение боя"""
-    if char_class == 'warrior':
-        print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
-    if char_class == 'mage':
-        print(f'{char_name}, ты Маг — превосходный укротитель стихий.')
-    if char_class == 'healer':
-        print(f'{char_name},'
-              f'ты Лекарь — чародей, способный исцелять раны.')
+def start_training(character):
+    """
+    Принимает на вход имя и класс персонажа.
+    Возвращает сообщения о результатах цикла тренировки персонажа.
+    """
+    print(character.__str__())
     print('Потренируйся управлять своими навыками.')
-    print('Введи одну из команд: attack — чтобы атаковать противника,'
-          'defence — чтобы блокировать атаку противника или special'
-          '— чтобы использовать свою суперсилу.')
+    print('Введи одну из команд: attack — чтобы атаковать противника, '
+          'defence — чтобы блокировать атаку противника или '
+          'special — чтобы использовать свою суперсилу.')
     print('Если не хочешь тренироваться, введи команду skip.')
-
     cmd = None
-
+    commands = {'attack': character.attack(),
+                'defence': character.defence(),
+                'special': character.special()
+                }
     while cmd != 'skip':
         cmd = input('Введи команду: ')
-        if cmd == 'attack':
-            print(attack(char_name, char_class))
-        if cmd == 'defence':
-            print(defence(char_name, char_class))
-        if cmd == 'special':
-            print(special(char_name, char_class))
+        if cmd in commands:
+            print(commands[cmd])
     return 'Тренировка окончена.'
 
 
@@ -132,4 +120,4 @@ if __name__ == '__main__':
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
     char_class = choice_char_class(char_name)
-    print(start_training(char_name, char_class))
+    print(start_training(char_class))
